ea1073344ddd008a1f02841253f16ec594500616
Evgeniy Krasnyanskiy <evgeniy.krasnyanskiy@gmail.com>
2026-06-03 19:24:31 +0300

    Удаление устаревшего скрипта start_notifier.sh

--- start_notifier.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Скрипт автоматического запуска Teragis Notifier на Linux (Xubuntu)
-# Переходим в директорию, где находится этот скрипт, и заходим в db_notifier
-cd "$(dirname "$0")"/db_notifier
-python3 tnotif.py
